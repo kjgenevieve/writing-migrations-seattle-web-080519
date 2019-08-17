8314bbca0e53477ea110133bff7762b755629c04
github username <github email address>
2019-08-16 17:07:58 -0700

    Done.

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,16 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+    def change
+        create_table :students do |t|
+            t.string :name
+        end
+    end
 end
+
+
+
+
+# Define a method called change and use the Active Record create_table method within that method to create the table. The table should have a :name column with a type string.
+
+# After you finish defining the change method, run the migrations by running rake db:migrate in your terminal.
+
+# bundle exec rake 
